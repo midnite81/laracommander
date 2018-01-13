3678ffbc5c3485d7b813245f816156caaaf707ba
Simon Rogers <webdev@centralblue.co.uk>
2018-01-13 17:35:15 +0000

    Update for backwards compatability

--- src/Routes/artisan.php
+++ src/Routes/artisan.php
@@ -1,15 +1,11 @@
 <?php
 
-
-
-Route::prefix(config('laracommander.prefix', 'console'))
-    ->middleware(config('laracommander.middleware', ['web']))
-    ->namespace('Midnite81\LaraCommander\Controllers')
-    ->group(function($router) {
-
-        $router->get('/', 'ConsoleController@index')->name('midnite81.artisan.dashboard');
-        $router->get('/{command}/view', 'ConsoleController@view')->name('midnite81.artisan.view');
-        $router->post('/{command}/execute', 'ConsoleController@execute')->name('midnite81.artisan.execute');
-
-    });
-
+Route::group([
+    'middleware' => config('laracommander.middleware', ['web']),
+    'namespace' => 'Midnite81\LaraCommander\Controllers',
+    'prefix' => config('laracommander.prefix', 'console'),
+], function($router) {
+    $router->get('/', 'ConsoleController@index')->name('midnite81.artisan.dashboard');
+    $router->get('/{command}/view', 'ConsoleController@view')->name('midnite81.artisan.view');
+    $router->post('/{command}/execute', 'ConsoleController@execute')->name('midnite81.artisan.execute');
+});
